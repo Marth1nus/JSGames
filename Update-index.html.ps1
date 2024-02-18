55bc08165a9002b491a6314949e860336c4e26c9
= <=>
2024-02-18 18:25:36 +0200

    Powershell automated index.html link generation

---
+++ Update-index.html.ps1
@@ -0,0 +1,8 @@
+(Get-Content .\index.html -Raw) -replace `
+('(\s*?)(<main id="project-links">)([\S\s]*?)(</main>)'), `
+('$1$2{0}$1$4' -f (
+    Get-ChildItem *.html -Exclude index.html |
+    ForEach-Object { $_.BaseName } |
+    ForEach-Object { "`$1`t<a href=`"$_.html`">$_</a>" } |
+    Join-String)) |
+Set-Content .\index.html
