e2c76bd7b3cd2708fe01aeddbe55bcbf138345b6
= <=>
2024-03-14 18:49:56 +0200

    Added trim to Update-index script to prevent the accumulating newlines at the end of index.html

--- Update-index.html.ps1
+++ Update-index.html.ps1
@@ -1,8 +1,11 @@
-(Get-Content .\index.html -Raw) -replace `
-('(\s*?)(<main id="project-links">)([\S\s]*?)(</main>)'), `
-('$1$2{0}$1$4' -f (
-    Get-ChildItem *.html -Exclude index.html |
-    ForEach-Object { $_.BaseName } |
-    ForEach-Object { "`$1`t<a href=`"$_.html`">$_</a>" } |
-    Join-String)) |
-Set-Content .\index.html
+Set-Content .\index.html (
+	(Get-Content .\index.html -Raw) -replace `
+	('(\s*?)(<main id="project-links">)([\S\s]*?)(</main>)'), `
+	('$1$2{0}$1$4' -f (
+			Get-ChildItem *.html -Exclude index.html |
+			ForEach-Object { $_.BaseName } |
+			ForEach-Object { "`$1`t<a href=`"$_.html`">$_</a>" } |
+			Join-String
+		)
+	)
+).Trim()
